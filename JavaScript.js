06ce1725f6845737c7c40405df13b7addaf196e0
Omar Hakem <youtarawneh997@gmail.com>
2020-12-16 12:04:03 +0200

    Finish the web page

---
+++ JavaScript.js
@@ -0,0 +1,16 @@
+var UserName = prompt("please Enter your name:")
+
+alert("Hello " + UserName + " Welcome to Pet Web Service.")
+
+var PetKind = prompt("What kind of Pet you're intrested in: Cats, Dogs, or Brids")
+
+if (PetKind == 'Cats'){
+    alert("Great feel free to read more about " + PetKind + " in our website")
+}else if (PetKind == 'Dogs'){
+    alert("Great feel free to read more about " + PetKind + " in our website")
+}else if (PetKind == 'Birds'){
+    alert("Great feel free to read more about " + PetKind + " in our website")
+}else {
+    alert("Please refresh the page and try to enter the pet name again.")
+}
+
